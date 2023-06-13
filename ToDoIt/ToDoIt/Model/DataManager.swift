9c5d0c3bb52b31844134d474dc3cc88b32577f3d
Caroline Frey <cfrey9@gmail.com>
2023-06-13 05:07:25 -0600

    Move task related classes from DataManager to TaskData file

--- ToDoIt/ToDoIt/Model/DataManager.swift
+++ ToDoIt/ToDoIt/Model/DataManager.swift
@@ -9,67 +9,6 @@ import Foundation
 import CoreData
 import UIKit
 
-enum EditMode {
-    case none, selectTasks, showCompletedTasks
-}
-
-class AllTasks {
-    var completedTasks: [ToDoItem]
-    var incompleteTasks: [ToDoItem] = []
-    
-    init() {
-        var temp: [ToDoItem] = []
-        DataManager.fetchTasks { tasks in
-            if let fetchedTasks = tasks {
-                temp = fetchedTasks
-            }
-        }
-        self.completedTasks = temp
-        
-        for task in temp {
-            if task.complete != true {
-                incompleteTasks.append(task)
-                completedTasks.removeAll { $0 == task }
-            }
-        }
-    }
-    
-    func markTaskComplete(task: ToDoItem) {
-        task.complete = true
-        incompleteTasks.removeAll { $0 == task }
-        completedTasks.append(task)
-        
-        DataManager.updateTask(toDoItem: task, task: task.task, tag: task.tag, complete: true)
-    }
-    
-    func markTaskIncomplete(task: ToDoItem) {
-        task.complete = false
-        completedTasks.removeAll { $0 == task }
-        incompleteTasks.append(task)
-        
-        DataManager.updateTask(toDoItem: task, task: task.task, tag: task.tag, complete: false)
-    }
-}
-
-class Tags {
-    var tasks: AllTasks
-    var tags: [String]
-    
-    init(tasks: AllTasks) {
-        var temp: [String] = []
-        self.tasks = tasks
-        // iterate through tasks and append tags to allTags array, avoiding dupes
-        for toDoItem in tasks.completedTasks {
-            if let taskTag = toDoItem.tag {
-                if taskTag != "" && !temp.contains(taskTag) {
-                    temp.append(taskTag)
-                }
-            }
-        }
-        self.tags = temp
-    }
-}
-
 class DataManager {
     static let managedObjectContext: NSManagedObjectContext = {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
